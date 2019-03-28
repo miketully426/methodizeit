c5dd5aef0a7829a9ce61e39a225d979a1066bbca
mtully426 <mike.m.tully@gmail.com>
2019-03-27 21:32:12 -0500

    moved almost everything into methods

--- src/com/company/MethodizeIt.java
+++ src/com/company/MethodizeIt.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class MethodizeIt {
 
+    //Prints the welcome message
     public static void welcomeMessage() {
         System.out.println("******** Welcome to Sign Me Up! ********");
     }
 
+    /*Prompts the user to chose if they want to associate their account with
+    a phone number or email.
+    Takes in a Scanner as a parameter and uses it to take in the input and
+    returns the user choice as an int
+    */
     public static int getChoice(Scanner s) {
         int choice;
         do {
@@ -20,49 +26,61 @@ public class MethodizeIt {
         return choice;
     }
 
+    /* Includes a series of prompts for the user to gather info to create the account
+    Takes in a Scanner and int of the choice for email or phone number as parameters, then
+    runs a series of methods to gather the data. Finally, returns a String array containing the
+    data provided by the user.
+     */
     public static String[] getUserData(Scanner s, int choice){
         String username, password, phoneEmail;
         if (choice == 1) {
             username = getUsername(s);
             password = getPassword(s);
-            phoneEmail = getPhoneNumber(s);
+            phoneEmail = getData(s, "Phone Number");
         } else {
             username = getUsername(s);
             password = getPassword(s);
-            phoneEmail = getEmail(s);
+            phoneEmail = getData(s, "Email");
         }
-        String[] userData = {username, password, phoneEmail};
-        return userData;
+        return new String[] {username, password, phoneEmail};
     }
 
-    public static String getData(Scanner s) {
-        String phoneEmail;
-        System.out.println("Enter your ");
-        phoneEmail = s.nextLine();
-        return phoneEmail;
-    }
-
-    public static String getPhoneNumber(Scanner s) {
-        String phoneEmail;
-        System.out.println("What is your phone number?");
-        phoneEmail = s.nextLine();
-        return phoneEmail;
-    }
-
-    public static String getPassword(Scanner s) {
-        String password;
-        System.out.println("What do you want your password to be?");
-        password = s.nextLine();
-        return password;
-    }
-
+    /* Takes in a Scanner as a parameter and prompts the user for a username.
+    Returns whatever the user types into the Scanner as a String.
+     */
     public static String getUsername(Scanner s) {
-        String username;
         System.out.println("What do you want your username to be?");
-        username = s.nextLine();
-        return username;
+        return s.nextLine();
+    }
+    /* Takes in a Scanner as a parameter and prompts the user for a password.
+    Returns whatever the user types into the Scanner as a String.
+    NOTE: This method could be potentially folded into the getUsername method and
+    structured similar to the getData method.
+     */
+    public static String getPassword(Scanner s) {
+        System.out.println("What do you want your password to be?");
+        return s.nextLine();
     }
 
+    /* Method to prompt the user for a piece of data, but the data that is asked for
+    isn't hard coded into the method. Instead, that piece of data being asked for (in this
+    case, phone number or email) is passed into the method via the parameters. The String dataType
+    parameter is plugged into the prompt to the user, and the result the user inputs is returned
+    as a String.
+     */
+    public static String getData(Scanner s, String dataType) {
+        System.out.println("What is your " + dataType + "?");
+        return s.nextLine();
+    }
+
+    /* Takes in a Scanner and prompts the user for their age. Since we never use age,
+    but only want to ensure they give us a value over 13, it repeatedly asks for an age
+    until a valid age is input. The method always returns true.
+    NOTE: In practice, prompting the user over and over to validate age makes no sense.
+    Instead, the return type on this method could be changed to void, and if the user enters
+    an invalid age, the program prints a message noting the invalid age and immediately ends.
+    Or if the user enters a valid age, the program is allowed to continue.
+     */
     public static boolean validateAge(Scanner s) {
         boolean validAge = false;
         do {
